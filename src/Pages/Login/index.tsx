920a79ed1fc969600f20c98ba3becab1f13862e1
Ajinkya710 <ajinkyabuva7@gmail.com>
2025-01-16 17:12:05 +1100

    updated login page css

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -74,33 +74,25 @@ const PageWrapper = styled.div`
   height: 100vh;
   justify-content: center;
   align-items: center;
+  background: rgba(0, 0, 0, 0.2);
 `;
 
 const Container = styled.div`
-  border: 1px solid rgba(200, 200, 200, 0.3);
-  border-radius: 1rem;
-  max-width: 500px;
+  max-width: 800px;
   width: 100%;
+  height: 500px;
   display: flex;
-  flex-direction: column;
+  gap: 1rem;
   align-items: center;
-  padding-bottom: 1rem;
-  background-color: #ffffff;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.3);
+  background-color: #000000;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 5px 10px, rgba(0, 0, 0, 0.23) 0px 5px 10px;
 `;
 
 const HeaderContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  border-top-right-radius: 1rem;
-  border-top-left-radius: 1rem;
   width: 100%;
-  background: linear-gradient(
-    58deg,
-    rgba(243, 172, 18, 1) 20%,
-    rgba(241, 196, 15, 1) 100%
-  );
   padding-top: 1rem;
 `;
 
@@ -119,17 +111,19 @@ const LightText = styled.p`
 const Label = styled.label``;
 
 const FormContainer = styled.form`
-  width: 90%;
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
+  justify-content: center;
   gap: 1rem;
-  padding: 1rem 0;
+  padding: 0 2rem;
+  background-color: #ffffff;
 `;
 
 const Input = styled.input`
   height: 42px;
   border: 1px solid rgba(200, 200, 200, 0.3);
-  border-radius: 5px;
   transition: all 200ms ease-in-out;
   outline: none;
   padding: 0 1rem;
@@ -141,7 +135,7 @@ const Input = styled.input`
 
   &:focus {
     outline: none;
-    border-bottom: 2px solid rgba(241, 196, 15, 1);
+    border-bottom: 2px solid rgba(24, 24, 23, 0.8);
   }
 `;
 
@@ -151,24 +145,19 @@ const ErrorText = styled.p`
 `;
 
 const SubmitButton = styled.button`
-  margin: 0 auto;
+  margin: 0.5rem auto;
   width: 100%;
   max-width: 150px;
-  padding: 10px;
+  padding: 1rem;
   color: #fff;
   font-size: 1rem;
   font-weight: 600;
   border: none;
-  border-radius: 100px;
   cursor: pointer;
   transition: all 240ms ease-in-out;
-  background: linear-gradient(
-    58deg,
-    rgba(243, 172, 18, 1) 20%,
-    rgba(241, 196, 15, 1) 100%
-  );
+  background: #000000;
 
   &:hover {
-    filter: brightness(1.05);
+    background: rgba(0, 0, 0, 0.6);
   }
 `;
